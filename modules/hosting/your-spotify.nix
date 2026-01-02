5c91c46390788bb0462215c4512ba3360413e55a
vicgeentor <victenbokum@gmail.com>
2026-01-02 21:26:53 +0100

    fixed typo

--- modules/hosting/your-spotify.nix
+++ modules/hosting/your-spotify.nix
@@ -1,14 +1,16 @@
 {
   flake.modules.nixos.your-spotify = nixosArgs: {
-    enable = true;
-    enableLocalDB = true;
-    spotifySecretFile = nixosArgs.config.age.secrets.your-spotify.path;
-    settings = {
-      SPOTIFY_PUBLIC = "11ce6df3ad974ebdb6c4d3c41728475b";
-      API_ENDPOINT = "https://apispoti.vicgeentor.nl";
-      CLIENT_ENDPOINT = "https://spoti.vicgeentor.nl";
-      PORT = 8080;
+    services.your_spotify = {
+      enable = true;
+      enableLocalDB = true;
+      spotifySecretFile = nixosArgs.config.age.secrets.your-spotify.path;
+      settings = {
+        SPOTIFY_PUBLIC = "11ce6df3ad974ebdb6c4d3c41728475b";
+        API_ENDPOINT = "https://apispoti.vicgeentor.nl";
+        CLIENT_ENDPOINT = "https://spoti.vicgeentor.nl";
+        PORT = 8080;
+      };
+      nginxVirtualHost = "localhost:80";
     };
-    nginxVirtualHost = "localhost:80";
   };
 }
