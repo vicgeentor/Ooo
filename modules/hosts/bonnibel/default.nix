4d754d4903ba2703753e76ce11a2a558f8745396
vicgeentor <github@vicgeentor.nl>
2026-01-12 13:13:11 +0100

    added tailscale exit node module and made bonnibel an exit node

--- modules/hosts/bonnibel/default.nix
+++ modules/hosts/bonnibel/default.nix
@@ -14,6 +14,7 @@
       pangolin
       ssh
       tailscale
+      tailscale-exit-node
     ];
   };
 }
---
+++ modules/system/tailscale-exit-node.nix
@@ -0,0 +1,11 @@
+{
+  flake.modules.nixos.tailscale-exit-node =
+    {
+      services.tailscale = {
+        useRoutingFeatures = "both";
+        extraUpFlags = [
+          "--advertise-exit-node"
+        ];
+      };
+    };
+}
