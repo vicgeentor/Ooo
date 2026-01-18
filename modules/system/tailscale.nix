ec0cc1c1a46182b122026c7e3b16d3fb479c03d6
vicgeentor <github@vicgeentor.nl>
2026-01-18 01:00:21 +0100

    Automated commit

--- modules/system/tailscale.nix
+++ modules/system/tailscale.nix
@@ -1,32 +1,18 @@
 {
-  flake.modules.nixos.tailscale =
-    nixosArgs@{ pkgs, ... }:
-    {
-      age.secrets.tailscale.file = ../../_secrets/tailscale.age;
+  flake.modules.nixos.tailscale = nixosArgs: {
+    age.secrets.tailscale.file = ../../_secrets/tailscale.age;
 
-      services.tailscale = {
-        enable = true;
-        useRoutingFeatures = "both";
-        authKeyFile = nixosArgs.config.age.secrets.tailscale.path;
-        extraUpFlags = [
-          "--netfilter-mode=nodivert"
-          "--ssh"
-        ];
-      };
-
-      # https://github.com/tailscale/tailscale/issues/3932
-      systemd.services.tailscaled.after = [ "wpa_supplicant.service" ];
-
-      # Start and stop command
-      environment.systemPackages = [
-        (pkgs.writeShellScriptBin "tailscale-start" ''
-          sudo systemctl start tailscaled
-          sudo tailscale up
-        '')
-        (pkgs.writeShellScriptBin "tailscale-stop" ''
-          sudo tailscale down
-          sudo systemctl stop tailscaled
-        '')
+    services.tailscale = {
+      enable = true;
+      useRoutingFeatures = "none";
+      authKeyFile = nixosArgs.config.age.secrets.tailscale.path;
+      extraUpFlags = [
+        "--netfilter-mode=nodivert"
+        "--ssh"
       ];
     };
+
+    # https://github.com/tailscale/tailscale/issues/3932
+    systemd.services.tailscaled.after = [ "wpa_supplicant.service" ];
+  };
 }
--- modules/system/vpn.nix
+++ modules/system/vpn.nix
@@ -4,9 +4,7 @@
     {
       networking.firewall.checkReversePath = false;
       environment.systemPackages = with pkgs; [
-        wireguard-tools
-        protonvpn-gui
-        proton-vpn-cli
+        eddie
       ];
     };
 }
