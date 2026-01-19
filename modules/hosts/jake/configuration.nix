2df1c892f5761740d17dc943aedcf03ffa3f8e61
vicgeentor <github@vicgeentor.nl>
2026-01-19 11:24:33 +0100

    using nixos-facter for jake host now

--- modules/hosts/jake/configuration.nix
+++ modules/hosts/jake/configuration.nix
@@ -1,32 +1,31 @@
-{ config, ... }:
 {
-  flake.modules.nixos.jake =
-    { pkgs, ... }:
-    {
+  flake.modules.nixos.jake = {
 
-      networking.hostName = "jake";
-      system.stateVersion = "25.05";
+    # These three are required
+    networking.hostName = "jake";
+    system.stateVersion = "25.05";
+    hardware.facter.reportPath = ./facter.json;
 
-      boot = {
-        loader.efi.canTouchEfiVariables = true;
-        extraModprobeConfig = ''
-          options snd_hda_intel power_save=0
-        '';
-        supportedFilesystems = [ "ntfs" ]; # Yes, I dual-boot Windows
-      };
-
-      hardware.nvidia.prime = {
-        intelBusId = "PCI:0:2:0";
-        nvidiaBusId = "PCI:1:0:0";
-      };
-
-      environment.etc."nsswitch.conf".text = ''
-        hosts: files dns
+    boot = {
+      loader.efi.canTouchEfiVariables = true;
+      extraModprobeConfig = ''
+        options snd_hda_intel power_save=0
       '';
-
-      services = {
-        libinput.enable = true;
-        printing.enable = true;
-      };
+      supportedFilesystems = [ "ntfs" ]; # Yes, I dual-boot Windows
     };
+
+    hardware.nvidia.prime = {
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+
+    environment.etc."nsswitch.conf".text = ''
+      hosts: files dns
+    '';
+
+    services = {
+      libinput.enable = true;
+      printing.enable = true;
+    };
+  };
 }
