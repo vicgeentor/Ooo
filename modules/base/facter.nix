6b953b21f0ab69d188c06aba2a07a1fd0b187d0c
vicgeentor <github@vicgeentor.nl>
2026-01-19 21:22:11 +0100

    facter

--- modules/base/facter.nix
+++ modules/base/facter.nix
@@ -1,3 +1,4 @@
+# sudo nix run --option experimental-features "nix-command flakes" nixpkgs#nixos-facter -- -o facter.json
 {
   flake.modules.nixos.base = {
     hardware.facter.detected.dhcp.enable = false;
--- modules/base/networking.nix
+++ modules/base/networking.nix
@@ -1,42 +1,44 @@
 {
-  flake.modules.nixos.base = {
-    networking = {
-      useDHCP = true; # NOTE: disable manually for specific hosts
-      networkmanager.dns = "systemd-resolved";
-      nameservers = [
-        "1.1.1.1#one.one.one.one"
-        "1.0.0.1#one.one.one.one"
-        "2606:4700:4700::1111#one.one.one.one"
-        "2606:4700:4700::1001#one.one.one.one"
+  flake.modules.nixos.base =
+    { lib, ... }:
+    {
+      networking = {
+        useDHCP = lib.mkDefault true; # NOTE: disable manually for specific hosts
+        networkmanager.dns = "systemd-resolved";
+        nameservers = [
+          "1.1.1.1#one.one.one.one"
+          "1.0.0.1#one.one.one.one"
+          "2606:4700:4700::1111#one.one.one.one"
+          "2606:4700:4700::1001#one.one.one.one"
 
-        "8.8.8.8#dns.google"
-        "8.8.4.4#dns.google"
-        "2001:4860:4860::8888#dns.google"
-        "2001:4860:4860::8844#dns.google"
-      ];
+          "8.8.8.8#dns.google"
+          "8.8.4.4#dns.google"
+          "2001:4860:4860::8888#dns.google"
+          "2001:4860:4860::8844#dns.google"
+        ];
+      };
+
+      services.resolved = {
+        enable = true;
+        dnssec = "true";
+        domains = [ "~." ];
+        fallbackDns = [
+          "1.1.1.1#one.one.one.one"
+          "1.0.0.1#one.one.one.one"
+          "2606:4700:4700::1111#one.one.one.one"
+          "2606:4700:4700::1001#one.one.one.one"
+
+          "8.8.8.8#dns.google"
+          "8.8.4.4#dns.google"
+          "2001:4860:4860::8888#dns.google"
+          "2001:4860:4860::8844#dns.google"
+        ];
+        dnsovertls = "true";
+      };
+
+      systemd = {
+        network.wait-online.enable = false;
+        services.NetworkManager-wait-online.enable = false;
+      };
     };
-
-    services.resolved = {
-      enable = true;
-      dnssec = "true";
-      domains = [ "~." ];
-      fallbackDns = [
-        "1.1.1.1#one.one.one.one"
-        "1.0.0.1#one.one.one.one"
-        "2606:4700:4700::1111#one.one.one.one"
-        "2606:4700:4700::1001#one.one.one.one"
-
-        "8.8.8.8#dns.google"
-        "8.8.4.4#dns.google"
-        "2001:4860:4860::8888#dns.google"
-        "2001:4860:4860::8844#dns.google"
-      ];
-      dnsovertls = "true";
-    };
-
-    systemd = {
-      network.wait-online.enable = false;
-      services.NetworkManager-wait-online.enable = false;
-    };
-  };
 }
