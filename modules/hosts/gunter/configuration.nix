9717a8eb7ba410cc50e6cbd700545bb01e5c3aa7
vicgeentor <github@vicgeentor.nl>
2026-01-19 11:32:38 +0100

    using nixos-facter for gunter host now

--- modules/hosts/gunter/configuration.nix
+++ modules/hosts/gunter/configuration.nix
@@ -1,8 +1,10 @@
 {
   flake.modules.nixos.gunter = {
 
+    # These three are required
     networking.hostName = "gunter";
     system.stateVersion = "26.05";
+    hardware.facter.reportPath = ./facter.json;
 
     boot.loader.efi.canTouchEfiVariables = true;
 
