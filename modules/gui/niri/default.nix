b401ea4525ac32bb147df07385c045dbeca47769
vicgeentor <github@vicgeentor.nl>
2026-09-07 16:19:38 +0200

    force use of gtk portal for file picker

--- modules/gui/niri/default.nix
+++ modules/gui/niri/default.nix
@@ -18,6 +18,10 @@
       trusted-substituters = [ "https://niri.cachix.org" ];
       trusted-public-keys = [ "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964=" ];
     };
+
+    xdg.portal.config.niri = {
+      "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+    };
   };
 
   flake.modules.homeManager.niri = hmArgs: {
